9ee321a2d60e95fe63df9cd99f7075e9b2d1a3cd
jiang696 <jiang696@gmail.com>
2016-11-17 17:12:39 -0800

    Modifying selecting data by names
    1. In empty_structure(), changing to dtype=dtype (pre-defined)
    2. Deleting the input "found" variable
    3. Changing input variable name "name" to "names"
    4. Finish selecting by names method

--- gal_data.py
+++ gal_data.py
@@ -12,64 +12,7 @@ def empty_gal_struct(n):
 
     dtype = (zip(columns, ts))
     empty = np.recarray((n,),
-                        dtype=[('name', object),
-                        ('pgc', int),
-                        ('alias', object),
-                        ('tags', object),
-                        ('dist_mpc', float),
-                        ('e_dist', float),
-                        ('ref_dist', object),
-                        ('vhel_kms', float),
-                        ('e_vhel_kms', float),
-                        ('ref_vhel', object),
-                        ('vrad_kms', float),
-                        ('e_vrad_kms', float),
-                        ('ref_vrad', object),
-                        ('vvir_kms', float),
-                        ('e_vvir_kms', float),
-                        ('ref_vvir', object),
-                        ('ra_deg', float),
-                        ('dec_deg', float),
-                        ('ref_pos', object),
-                        ('posang_deg', float),
-                        ('e_posang', float),
-                        ('ref_posang', object),
-                        ('incl_deg', float),
-                        ('e_incl', float),
-                        ('ref_incl', object),
-                        ('log_raxis', float),
-                        ('e_log_raxis', float),
-                        ('ref_log_raxis', object),
-                        ('t', float),
-                        ('e_t', float),
-                        ('ref_t', object),
-                        ('morph', object),
-                        ('bar', int),
-                        ('ring', int),
-                        ('multiple', int),
-                        ('ref_morph', object),
-                        ('av_sf11', float),
-                        ('r25_deg', float),
-                        ('e_r25_deg', float),
-                        ('ref_r25', object),
-                        ('vmaxg_kms', float),
-                        ('e_vmaxg_kms', float),
-                        ('ref_vmaxg', object),
-                        ('vrot_kms', float),
-                        ('e_vrot_kms', float),
-                        ('ref_vrot', object),
-                        ('hi_msun', float),
-                        ('ref_hi', object),
-                        ('lfir_lsun', float),
-                        ('ref_ir', object),
-                        ('btc_mag', float),
-                        ('ref_btc', object),
-                        ('ubtc_mag', float),
-                        ('ref_ubtc', object),
-                        ('bvtc_mag', float),
-                        ('ref_bvtc', object),
-                        ('itc_mag', float),
-                        ('ref_itc', object)] )
+                        dtype=dtype)
 
     for i in range(len(empty)):
         for j in range(len(empty[i])):
@@ -78,9 +21,9 @@ def empty_gal_struct(n):
     return empty
 
 
-def gal_data(name=None, data=None, all=False, data_dir=None, found=None, tag=None):
+def gal_data(names=None, data=None, all=False, data_dir=None, tag=None):
 
-    if not name and not all and not tag:
+    if not names and not all and not tag:
         print('Need a name to find a galaxy. Returning empty structure')
         return None
 
@@ -119,38 +62,31 @@ def gal_data(name=None, data=None, all=False, data_dir=None, found=None, tag=Non
 
         good_data = data[np.where(keep)]
 
-        return data[np.where(keep)]
+        return good_data
 
+    # BUILD ALIAS DICTIONARY
+    aliases = {}
+    fname = os.path.join(data_dir, "gal_base_alias.txt")
+    f = open(fname)
+    f.readline()
+    f.readline()
+    for line in f:
+        s = [temp for temp in line.strip('\n').split(' ')]
+        aliases[s[0].replace(' ', '').upper()] = s[1].replace(' ', '').upper()
 
-    # NAME OR LIST OF NAMES
-    alias_vec, name_vec = np.loadtxt(os.path.join(data_dir, 'gal_base_alias.txt'), dtype='string', unpack=True)
-    alias_vec = [a.replace(' ', '').upper() for a in alias_vec]
-    name_vec = [a.replace(' ', '').upper() for a in name_vec]
+    # NAME OR NAMES of GALAXIES
+    if type(names) == str:
+        names = [names]
+    keep = np.zeros(len(data), dtype=bool)
 
+    # SEARCH FOR GALAXIES
+    for name in names:
+        name_a = aliases[name.replace(' ', '').upper()]
+        ind = data.field('NAME') == name_a
 
-    # IDENTIFY THE GALAXY
-    name = [name]
-    n_names = len(name)
-    output = empty_gal_struct(n_names)
-    found = np.ones(n_names)
+        if sum(ind) == 0:
+            print('No match for ' + name)
+        else:
+            keep += ind
 
-    name = np.asarray([a.replace(' ', '').upper() for a in name])
-    name_vec = np.asarray([a.replace(' ', '').upper() for a in name_vec])
-    alias_vec = np.asarray([a.replace(' ', '').upper() for a in alias_vec])
-    data_name = np.asarray([a.replace(' ', '').upper() for a in data.field('name')])
-
-    for i in range(n_names):
-        ind = np.where(alias_vec == name[i])[0]
-
-        if len(ind) == 0:
-            print('No match for ' + name[i])
-            found[i] = 0
-            continue
-
-        data_ind = np.where(data_name == name_vec[ind][0])[0]
-        output[i] = data[data_ind]
-        found[i] = 1
-
-    return output
-
-
+    return data[keep]
